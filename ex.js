52d4971a4ab65cddf0204910ac0a924f95ed9f22
Pablo Plata <pplata1631@gmail.com>
2018-05-02 21:50:17 -0500

    keep pushing

--- ex.js
+++ ex.js
@@ -1,16 +1,17 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+
 var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
         password: "P@ssW0rd",
         database: "bamazon"
-    })
+    });
 
 connection.connect(function(err){
-    if(err) throw err;
-    console.log("Connections is Working");
+    if (err) throw err;
+    console.log("Connection is Working");
    
 })
