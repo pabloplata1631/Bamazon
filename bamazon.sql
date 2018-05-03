651ce4d3e7b2df31dac4bd7c142eeeed9b7fa959
Pablo Plata <pplata1631@gmail.com>
2018-05-02 21:25:23 -0500

    2nd sql try

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon ;
+USE bamazon;
+
+CREATE TABLE products (
+item_id INTEGER AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price DECIMAL(10,4) NOT NULL,
+stock_quantity INTEGER (10) NOT NULL,
+PRIMARY KEY (item_id)
+);
+
