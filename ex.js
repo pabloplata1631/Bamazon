17de7b05f60f04d593e0a78a542a49a094b67e34
Pablo Plata <pplata1631@gmail.com>
2018-05-03 00:14:47 -0500

    adding table functi

--- ex.js
+++ ex.js
@@ -13,5 +13,15 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if (err) throw err;
     console.log("Connection is Working");
+    table();
    
 })
+
+// table function from mysql databe 
+var table = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for(var i=0; i <res.length; i++){
+            console.log(res[i].item_id+" || "+res[i].product_name+" || "+res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
+        }
+    })
+}
